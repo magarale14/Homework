f8d376ed389d16e961c7cbadac327f6bcde2327c
Reeta Ale Magar <41391452+magarale14@users.noreply.github.com>
2018-08-09 18:05:07 -0400

    Initial commit

---
+++ Arrays.java
@@ -0,0 +1,27 @@
+
+public class Arrays {
+	public static void main(String args[]){
+		
+		long[]l = new long[6];
+	    short[]s = new short[6];
+		char[]c = new char[6];
+		byte[]b = new byte[6];
+		float[]f = new float[6];
+		double[]d = new double[6];
+		boolean boo[] = new boolean[] {true,false};
+		String[]st = new String []{"tina","luna","tuna","ee","ii","rr"};
+		
+ 
+		int[] i = new int[] { 5, 8, 22, 9, 3, 48 }; // initializing values to int//
+		for (int x = 0; x < i.length; x+=2) //Using for loop to find even index value for int//
+		        System.out.println(i[x]);
+		    
+				
+	  for(String str: st){
+			   System.out.println(str);
+			 			  
+ }	
+          }
+	}
+//Using for loop to find even index value for int//
+   	
